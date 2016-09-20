b2c187b7eb72d8ffc7ee9fe55081658f0b1e5916
j376787348 <j376787348@163.com>
2016-09-20 23:43:03 +0800

    1.service的用法

--- servicedemo/src/main/java/com/syl/servicedemo/MainActivity.java
+++ servicedemo/src/main/java/com/syl/servicedemo/MainActivity.java
@@ -2,7 +2,12 @@ package com.syl.servicedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ * author   j3767
+ * date     2016/9/20 23:42
+ * desc
+ * 
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
